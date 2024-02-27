42683e88602ad0755a362650c3694a79943d554e
nhkim <nhkim1529@gmail.com>
2024-02-27 10:38:05 +0900

    [Feat] BottomNavigation + Navgraph

---
+++ app/src/main/java/com/example/navgraph/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.navgraph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.navgraph.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+}
